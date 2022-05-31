4d90a16aafac13146f6c80c2b52cf08f08395037
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2022-06-01 02:27:43 +0900

    add deno

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   description = "A collection of flake templates";
   outputs = { self, nixpkgs }: {
     templates = {
+      deno = {
+        path = ./deno;
+        description = "Deno template";
+      };
       nodejs = {
         path = ./nodejs;
         description = "Nodejs template";
